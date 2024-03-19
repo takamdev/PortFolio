8259df988902704a2548de24b17d213fb553e3b7
takamdev <panguslariss@gmail.com>
2024-03-19 19:45:52 +0100

    modif

--- src/componnents/About.jsx
+++ src/componnents/About.jsx
@@ -2,18 +2,16 @@ import { useId } from "react";
 
 function About() {
    return (
-      <section id="about" className="pt-5">
+      <section id="about" className="pt-2 pt-lg-0 container-fluid">
          <div className="row">
-            <div className="col-sm-12 col-sm-6 col-lg-6 justify-content-center ">
-               <img src="/images/toff.png" className=" w-100 h-75 ms-auto me-auto toff"  alt="image" />
-            </div>
-            <div className="col-sm-12 col-sm-6 col-lg-6  pt-5 ps-5">
+           
+            <div className="col-sm-12 col-sm-6 col-lg-6 ps-5">
                <p className="heading"  style={{ textAlign: "justify" }}>
                   <span className="display-6 fw-bold">
                      About <span style={{ color: "#0EF" }}>Me</span>
                   </span>{" "}
                   <br />
-                  <span className="display-5 fw-bolder">
+                  <span className=" display-6 fw-bolder">
                      {" "}
                      Front End Developper!
                   </span>{" "}
@@ -35,9 +33,11 @@ function About() {
                   Read More
                </a>
             </div>
+            <div className="col-sm-12 col-sm-6 col-lg-6 justify-content-center ">
+               <img src="/images/toff.png" className=" w-100 h-75 ms-auto me-auto toff"  alt="image" />
+            </div>
          </div>
       </section>
    );
 }
-
 export default About;
