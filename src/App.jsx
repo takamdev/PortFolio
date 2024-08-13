81810d3f83e9b029a36d9f2e6dfd0cd98aeacca7
Loic <panguslariss@gmail.com>
2024-08-13 18:48:45 +0100

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import NavBar from "./componnents/NavBar.jsx";
 import Home from "./componnents/Home.jsx";
 import About from "./componnents/About.jsx";
